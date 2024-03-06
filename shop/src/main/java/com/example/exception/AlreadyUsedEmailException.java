f55e66509137ec1e941b39c579d2408c282fdde5
Hey-Yujin365 <realgyj@gmail.com>
2024-03-06 18:25:07 +0900

    Spring-shop/ spring security추가, ResultMap, users관련 데이터 생성

---
+++ shop/src/main/java/com/example/exception/AlreadyUsedEmailException.java
@@ -0,0 +1,11 @@
+package com.example.exception;
+
+public class AlreadyUsedEmailException extends ShopException {
+
+	private static final long serialVersionUID = 6974693762699709632L;
+
+	public AlreadyUsedEmailException(String message) {
+		super(message);
+	}
+	
+}
--- shop/src/main/java/com/example/web/controller/HomeController.java
+++ shop/src/main/java/com/example/web/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.web.controller;
 
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,9 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.exception.AlreadyUsedEmailException;
+import com.example.exception.AlreadyUsedIdException;
+import com.example.service.UserService;
 import com.example.web.form.UserRegisterForm;
 
-import jakarta.validation.Valid;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
@@ -25,8 +28,10 @@ import lombok.extern.log4j.Log4j2;
  * 			log.error(메세지);
  * 		- System.out.println() 대신 반드시 log를 사용하자.
  */
-@Log4j2
+@RequiredArgsConstructor
 public class HomeController {
+	
+	private final UserService userService;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -46,6 +51,22 @@ public class HomeController {
 		if (errors.hasErrors()) {
 			return "form";
 		}
+		
+		try {
+			// 폼 입력값 유효성 체크를 통과한 경우
+			userService.registerUser(form);
+		} catch (AlreadyUsedIdException ex) {
+			// 이미 사용중인 아이디인 경우, 유효성 체크를 통과하지 못한 것으로 간주한다.
+			// rejectValue(필드명, 에러코드, 에러메세지)메소드는 BindingResult 객체에 FieldError를 추가한다.
+			// 에러코드를 정의하지 않았기 때문에 에러메세지를 넣어보낸다.
+			// 에러메세지가 표시되어야 하기 때문에 입력폼 화면으로 내부이동 시킨다.
+			errors.rejectValue("id", null, ex.getMessage());
+			return "form";
+		} catch (AlreadyUsedEmailException ex) {
+			errors.rejectValue("email", null, ex.getMessage());
+			return "form";
+		}
+		
 		return "redirect:/";
 	}
 }
