bebd89d885b293ff32a80031a64477718b1adc40
Hey-Yujin365 <realgyj@gmail.com>
2024-03-15 17:31:34 +0900

    Spring/관리자-상품-상세보기 모달창 수정폼 완성(ResponseBody - Json형태 - Controller 역직렬화)

--- shop/src/main/java/com/example/web/controller/AdminController.java
+++ shop/src/main/java/com/example/web/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -60,12 +61,6 @@ public class AdminController {
 		return "redirect:/admin/product/list";
 	}
 	
-	@GetMapping("/product/{productNo}")
-	@ResponseBody
-	public Product product(@PathVariable("productNo") int productNo) {
-		return productService.getProduct(productNo);
-	}
-	
 	@GetMapping("/user/list")
 	public String users(Model model) {
 		List<User> users = userService.getAllUsers();
@@ -102,5 +97,17 @@ public class AdminController {
 	@ResponseBody
 	public List<ProductCategory> categories(@RequestParam("catNo") int catNo) {
 		return productService.getAllSubProductCategories(catNo);
+	}
+	
+	@GetMapping("/product/{productNo}")
+	@ResponseBody
+	public Product getProduct(@PathVariable("productNo") int productNo) {
+		return productService.getProduct(productNo);
+	}
+	
+	@PostMapping("/product/modify")
+	@ResponseBody
+	public void modifyProduct(@RequestBody Product product) {
+		productService.updateProduct(product);
 	}
 }
