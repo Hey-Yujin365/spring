20814b6689696acff971fa97c364079651811971
Hey-Yujin365 <realgyj@gmail.com>
2024-03-28 12:46:59 +0900

    MemberRequest에 toString 어노테이션 추가

--- rest/src/main/java/com/example/member/MemberRequest.java
+++ rest/src/main/java/com/example/member/MemberRequest.java
@@ -5,9 +5,11 @@ import java.time.LocalDateTime;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class MemberRequest {
 
 	@Schema(description = "이메일", example = "hong@gmail.com")
